57f60f51c3c5f3f695685a4e83b4fedbfeccea2c
chandra.mouli <chandra.mouli@servicenow.com>
2021-01-02 18:49:56 +0530

    simple json payload

--- echoserver/simpleserv.go
+++ echoserver/simpleserv.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/ckharide/metainfo"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -19,9 +21,34 @@ func main() {
 }
 
 func echoJSONPayload(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Chandra Mouli New")
+	/*postBody, _ := json.Marshal(map[string]string{
+		"name":  "test",
+		"email": "Toby@example.com",
+	})*/
+
 	data := metainfo.Message{Name: "Chandra", Body: "Living in Hyd", Time: time.Now().UnixNano() / int64(time.Millisecond)}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(data)
+	b, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	responseBody := bytes.NewBuffer(b)
+	//Leverage Go's HTTP Post function to make request
+	resp, err := http.Post("https://postman-echo.com/post", "application/json", responseBody)
+	//Handle Error
+	if err != nil {
+		fmt.Println("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Fprintf(w, "Hello Chandra Mouli New")
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	sb := string(body)
+	fmt.Fprintf(w, sb)
+	//data := metainfo.Message{Name: "Chandra", Body: "Living in Hyd", Time: time.Now().UnixNano() / int64(time.Millisecond)}
+	//w.Header().Set("Content-Type", "application/json")
+	//w.WriteHeader(http.StatusCreated)
+	//json.NewEncoder(w).Encode(data)
 }
